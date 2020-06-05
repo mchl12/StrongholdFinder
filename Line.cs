e470ff9b7166cdfe280275be452b8705a1216452
Michel van der Lans <12mchl.vdl@gmail.com>
2020-06-05 19:37:54 +0200

    Initial Commit + first attempt

---
+++ Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StrongholdFinder
+{
+    class Line
+    {
+        double _slope;
+        double _intercept;
+
+        public void Intercept(Line other, out double x, out double z)
+        {
+            double slopeDiff = _slope - other._slope;
+            double interceptDiff = other._intercept - _intercept;
+
+            x = interceptDiff / slopeDiff;
+            z = _slope * x + _intercept;
+        }
+
+        public Line(double x, double z, double angle)
+        {
+            _slope = Math.Tan(angle);
+            _intercept = z - _slope * x;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StrongholdFinder
+{
+    class Program
+    {
+        static void GetData(out double x, out double z, out double angle)
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+
+                if (input.Length < 3)
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+
+                try
+                {
+                    x = double.Parse(input[0]);
+                    z = double.Parse(input[1]);
+                    angle = double.Parse(input[2]);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("invalid input");
+                    continue;
+                }
+                    
+                break;
+            }
+        }
+
+        static void Main()
+        {
+            GetData(out double ax, out double az, out double alpha);
+            GetData(out double bx, out double bz, out double beta);
+
+            // convert to radians
+            alpha *= Math.PI / 180;
+            beta *= Math.PI / 180;
+
+            Line l = new Line(ax, az, alpha);
+            Line m = new Line(bx, bz, beta);
+
+            l.Intercept(m, out double x, out double z);
+
+            int blockX = (int)x;
+            int blockZ = (int)z;
+
+            Console.WriteLine($"stronghold at {blockX} {blockZ}");
+        }
+    }
+}
